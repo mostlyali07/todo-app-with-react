e93dd580d004598a5b3436b55daa00543c21b972
Syed Ali Rehan <104094853+mostlyali07@users.noreply.github.com>
2022-11-25 03:14:42 +0500

    update

--- src/todoApp.js
+++ src/todoApp.js
@@ -1,19 +1,50 @@
-import React from 'react';
-import './App.css';
-import Upperimg from './images/notes.png';
+import React from "react";
+import "./App.css";
+import Upperimg from "./images/notes.png";
 
 const TodoApp = () => {
   return (
     <>
-       <div className="main_div">
-        <div className="child-div">
-            <figure>
+      <div className="container">
+        <div className="row">
+          <div className="col-md-4"></div>
+          <div className="col-md-4">
+            <div className="main_div">
+              <figure>
                 <img src={Upperimg} alt="Notes" className="notes" />
-            </figure>
-        </div>
-        </div> 
-    </>
-  )
-}
+                <figcaption>Add Todo List Here 👇</figcaption>
+              </figure>
+              <div className="addItems">
+                <div className="form-floating">
+                  <input
+                    type="text"
+                    className="form-control"
+                    id="floatingInput"
+                    placeholder="name@example.com"
+                  />
+                  <label for="floatingInput">Enter Items Here</label>
+                </div>
+                <button type="submit" class="btn btn-primary mb-3">
+                  <i class="fa-solid fa-folder-plus"></i>
+                </button>
+              </div>
 
-export default TodoApp
+              <div className="showItems">
+                <div className="eachItems">
+                  <h3>Shantu</h3>
+                  <i class="fa-solid fa-trash"></i>
+                </div>
+              </div>
+              <button type="submit" class="btn btn-danger mb-3">
+                Delete All <i class="fa-solid fa-delete-left"></i>
+              </button>
+            </div>
+          </div>
+          <div className="col-md-4"></div>
+        </div>
+      </div>
+    </>
+  );
+};
+
+export default TodoApp;
