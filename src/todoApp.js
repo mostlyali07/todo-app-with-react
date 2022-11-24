d423fa096b1cd96b98a95eda8a0acf4c07eb5472
Syed Ali Rehan <104094853+mostlyali07@users.noreply.github.com>
2022-11-25 03:32:09 +0500

    update

--- src/todoApp.js
+++ src/todoApp.js
@@ -1,8 +1,15 @@
-import React from "react";
+import React, {useState} from "react";
 import "./App.css";
 import Upperimg from "./images/notes.png";
 
 const TodoApp = () => {
+
+  const [inputData, setInputData] = useState('');
+  const [items, setItems] = useState([]);
+  const addItem =() =>{
+
+  }
+
   return (
     <>
       <div className="container">
@@ -21,22 +28,24 @@ const TodoApp = () => {
                     className="form-control"
                     id="floatingInput"
                     placeholder="name@example.com"
+                    value={inputData}
+                    onChange={(e) => setInputData(e.target.value)}
                   />
-                  <label for="floatingInput">Enter Items Here</label>
+                  <label htmlFor="floatingInput">Enter Items Here</label>
                 </div>
-                <button type="submit" class="btn btn-primary mb-3">
-                  <i class="fa-solid fa-folder-plus"></i>
+                <button type="submit" className="btn btn-primary mb-3" onClick={addItem}>
+                  <i className="fa-solid fa-folder-plus"></i>
                 </button>
               </div>
 
               <div className="showItems">
                 <div className="eachItems">
                   <h3>Shantu</h3>
-                  <i class="fa-solid fa-trash"></i>
+                  <i className="fa-solid fa-trash"></i>
                 </div>
               </div>
-              <button type="submit" class="btn btn-danger mb-3">
-                Delete All <i class="fa-solid fa-delete-left"></i>
+              <button type="submit" className="btn btn-danger mb-3">
+                Delete All <i className="fa-solid fa-delete-left"></i>
               </button>
             </div>
           </div>
